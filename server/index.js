ccbe9e18e13180b0d72b820d14c395786b98a05a
mjr2595 <michaeljohnraymond@gmail.com>
2023-04-10 10:50:54 -0500

    primero

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import { Express } from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// CONFIGS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
